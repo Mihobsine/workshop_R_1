dc2ddf49ccceb4b67510220b69bdfb07e99a8396
Valentin Ledesert <mihobsine@macbook-pro-de-valentin.home>
2021-02-17 09:42:34 +0100

    Add default files

--- step1.R
+++
@@ -1,8 +0,0 @@
-# Title     : TODO
-# Objective : TODO
-# Created by: mrsquaare
-# Created on: 15/05/2020
-
-dplyr::starwars
-
-select(starwars, -c("films", "vehicles", "starships"))
--- step2.R
+++
@@ -1,8 +0,0 @@
-# Title     : TODO
-# Objective : TODO
-# Created by: mrsquaare
-# Created on: 15/05/2020
-
-dplyr::starwars
-
-filter(starwars, gender == "female")
